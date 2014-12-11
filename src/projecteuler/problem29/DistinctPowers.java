c730219dbb5b3b6e412df82ffb9dd8772d78df4c
james-norcross <james.norcross@gmail.com>
2014-12-11 10:13:27 -0600

    initial commit

---
+++ src/projecteuler/problem29/DistinctPowers.java
@@ -0,0 +1,16 @@
+package projecteuler.problem29;
+
+public class DistinctPowers {
+
+	/**
+	 * Author: James Norcross
+	 * Date: 12/12/14
+	 * Purpose: Project Euler Problem 29
+	 * Description: Counts the distinct values of a list consisting of all the integers generated from the expression
+	 * a^b where 2<=a<=100 and 2<=b<=100
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
