f9a797d1d49a6fcf6b1d6028eb98943243d260e4
james-norcross <james.norcross@gmail.com>
2014-12-24 12:27:23 -0600

    final solution

--- src/projecteuler/problem29/DistinctPowers.java
+++ src/projecteuler/problem29/DistinctPowers.java
@@ -1,5 +1,10 @@
 package projecteuler.problem29;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class DistinctPowers {
 
 	/**
@@ -8,9 +13,22 @@ public class DistinctPowers {
 	 * Purpose: Project Euler Problem 29
 	 * Description: Counts the distinct values of a list consisting of all the integers generated from the expression
 	 * a^b where 2<=a<=100 and 2<=b<=100
+	 * 
 	 */
 	public static void main(String[] args) {
+		
+		
+		HashSet<BigInteger> powerSet = new HashSet<BigInteger>();	// note using hashset insures no duplicates
+		
+		for (int a = 2; a<=100; a++) {
+			for (int b = 2; b<=100; b++) {
+				powerSet.add(BigInteger.valueOf(a).pow(b));
+			}
+		}
 
+		System.out.println("The number of distinct terms is " + powerSet.size());
+		
 	}
+
 
 }
